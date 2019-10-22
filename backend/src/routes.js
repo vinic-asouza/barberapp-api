a9ac965d357fd33d4d88f42063a63a35a45fca96
Prisma MBP 103 <prisma@Assis.local>
2019-10-22 11:31:04 -0300

    feat[schedule]: create function INDEX in /ScheduleController to show (GET) the Schedule to PROVIDERS

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,7 @@ import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -25,5 +26,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
