52cccff9ea3368f4380adee239a4cf5267273926
Prisma MBP 103 <prisma@Assis.local>
2019-10-22 16:49:23 -0300

    feat[notification]: create Notifications to Appointments, using MongoDB

---
+++ backend/src/app/schemas/Notification.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const NotificationSchema = new mongoose.Schema(
+    {
+        content: {
+            type: String,
+            required: true,
+        },
+        user: {
+            type: Number,
+            required: true,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default mongoose.model('Notification', NotificationSchema);
