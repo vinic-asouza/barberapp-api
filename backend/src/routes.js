36e93ef033222139323fb56bda7ba4626b40a618
Prisma MBP 103 <prisma@Assis.local>
2019-10-23 14:23:01 -0300

    feat[queues]: create queues of jobs to performing response time. Using bee-queue

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -10,6 +10,7 @@ import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,6 +24,8 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/appointments', AppointmentController.store);
