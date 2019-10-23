de353202d0176bdc7c5208c564021517e8c528bd
Vinicius Souza <vinic.asouza@gmail.com>
2019-10-22 21:23:30 -0300

    feats[mail]: config email send wih nodemailer

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '3fcf501adaa5de',
+    pass: 'f5f21ebbad541a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>' 
+  }
+}
